98e31ada31c61dc3320fe1f18804c1666a899800
Vitor <vitorfonsecaxs@gmail.com>
2024-12-02 11:00:56 -0300

    Add: conexao db criada com sucesso

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,7 +12,49 @@ namespace projeto_agenda_telefonica.Controllers
         public bool CriarUsuario(string nome, string usuario, string telefone, string senha)
         {
             // Criando Conexão
-            MySqlConnection 
+            MySqlConnection conexao = null;
+
+            try
+            {
+                conexao.Open();
+
+                MySqlCommand add_usuario = new MySqlCommand(
+                     $@"
+                        INSERT INTO tb_usuarios VALUES (@pecado, @nome, @usuario, @telefone, @senha);
+                        CREATE USER '{usuario}'@'%' IDENTIFIED BY '{senha}';
+                        GRANT SELECT ON db_agenda.tb_categorias TO '{usuario}'@'%';
+                    ",
+                     conexao
+                     );
+
+                add_usuario.Parameters.AddWithValue("@nome", nome);
+                add_usuario.Parameters.AddWithValue("@usuario", usuario);
+                add_usuario.Parameters.AddWithValue("@telefone", telefone);
+                add_usuario.Parameters.AddWithValue("@senha", senha);
+
+                // retorna a quantidade de linhas afetadas 
+                if (add_usuario.ExecuteNonQuery() > 0)
+                {
+                    // deu certo o usuario foi inserido
+
+                    return true;
+                }
+                else
+                {
+                    // usuario nao cadastrado
+
+                    return false;
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show($"erro ao tentar cadastrar {erro.Message}");
+                return false;
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
     }
 }
---
+++ Data/ConexaoDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace projeto_agenda_telefonica.Data
+{
+    internal static class ConexaoDB
+    {
+        public static MySqlConnection Conexao()
+        {
+            // String contendo as credenciais para a conexão SQL
+            string comando = "Server=localhost;Database=db_agenda;User ID=root;Password=root;";
+
+            // Instanciando a conexao
+            MySqlConnection connection = new MySqlConnection(comando);
+
+            return connection;
+        }
+
+        public static MySqlConnection? Conexao(string usuario, string senha)
+        {
+            string comando = $"Server=localhost;Database=db_agenda;User ID={usuario};Password={senha};";
+
+            MySqlConnection connection = new MySqlConnection(comando);
+
+            try
+            {
+                // Validando a conexão
+                connection.Open();
+
+                return connection;
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+                return null;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
